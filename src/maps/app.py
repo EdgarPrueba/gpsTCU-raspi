1269a0d5f8d22848170b028866379dbf6d9d650b
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2025-03-18 22:01:25 -0600

    Cambios temporales para pruebas con mapa local.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- src/maps/app.py
+++ src/maps/app.py
@@ -1,26 +1,33 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import csv
 import random
 import time
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 @app.route('/')
 def index():
     return render_template('map.html')
 
+
 def send_gps_data():
-    lat, lon = 40.758896, -73.985130  # Coordenadas iniciales (Ejemplo: NYC)
-    while True:
-        lat += random.uniform(-0.0005, 0.0005)  # Simulación de movimiento
-        lon += random.uniform(-0.0005, 0.0005)
-        socketio.emit('gps_update', {'lat': lat, 'lon': lon})
-        time.sleep(1)  # Enviar cada 1s
+    with open('datos_gps.csv', 'r') as csvfile:
+        content = csv.reader(csvfile)
+
+        for row in content:
+            lat = float(row[0])
+            lon = float(row[1])
+            socketio.emit('gps_update', {'lat': lat, 'lon': lon})
+            time.sleep(1)  # Enviar cada 1s
+
 
 @socketio.on('connect')
 def handle_connect():
     print("Cliente conectado")
+
 
 if __name__ == '__main__':
     socketio.start_background_task(send_gps_data)
